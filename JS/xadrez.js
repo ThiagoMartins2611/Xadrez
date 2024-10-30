6cbc015d056aca90b3601146075db976de93fa2e
Thiago Costa Martins <thiagogames2611@gmail.com>
2024-10-30 17:51:34 -0300

    Add files via upload

--- JS/xadrez.js
+++ JS/xadrez.js
@@ -2,9 +2,17 @@ let xadrez = document.getElementById('xadrez')
 
 for(let i=0; i<64; i++){
     let squares = document.createElement("div");
+    let black = "black squares"
+    let white = "white squares"
 
-    squares.className = "black squares"
+    let quadradoAnterior = squares.className;
 
+    if(quadradoAnterior != white){
+        squares.className = white;
+    }else{
+        squares.className = black;
+    }
+    
     xadrez.appendChild(squares);
 
 }
